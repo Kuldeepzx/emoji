d2d393743904d2b61b1c47b4f4b6a01cd69d6955
patelcool <patelkuldeep349@gmail.com>
2020-02-20 04:40:11 -0800

    first commit

---
+++ src/filterEmoji.js
@@ -0,0 +1,17 @@
+import emojiList from "./emojiList.json";
+
+export default function filterEmoji(searchText, maxResults) {
+  return emojiList
+    .filter(emoji => {
+      if (emoji.title.toLowerCase().includes(searchText.toLowerCase())) {
+        return true;
+      }
+      if (emoji.keywords.includes(searchText)) {
+        return true;
+      }
+      return false;
+    })
+    
+    .slice(0, maxResults);
+    
+}
